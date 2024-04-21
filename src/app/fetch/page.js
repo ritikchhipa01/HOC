544f3f4bc963acaa7a368720407bc04bf4c2c231
ritikchhipa01 <ritikchhipa01@gmail.com>
2024-04-22 00:54:45 +0530

    one positivde  try

--- src/app/fetch/page.js
+++ src/app/fetch/page.js
@@ -1,26 +1,26 @@
-"use client"
+// "use client"
 
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
-const page = () => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const communitydata = useSelector((store) => store.data);
-    // console.log(communitydata[0]);
+// const page = () => {
+//     // eslint-disable-next-line react-hooks/rules-of-hooks
+//     const communitydata = useSelector((store) => store.data);
+//     // console.log(communitydata[0]);
 
-    return (
-        <div className=' h-screen my-10'>
-            {communitydata[0] &&
-                communitydata[0].map((user, index) => {
-                    return (<div key={index} className='mb-4 text-white text-4xl flex justify-center gap-2'>
-                        <p>{user.CommunityName}</p>
-                        <p>{user.description}</p>
-                        <p>{user.builder}</p>
-                    </div>
-                    )
-                })
-            }
-        </div>
-    )
-}
+//     return (
+//         <div className=' h-screen my-10'>
+//             {communitydata[0] &&
+//                 communitydata[0].map((user, index) => {
+//                     return (<div key={index} className='mb-4 text-white text-4xl flex justify-center gap-2'>
+//                         <p>{user.CommunityName}</p>
+//                         <p>{user.description}</p>
+//                         <p>{user.builder}</p>
+//                     </div>
+//                     )
+//                 })
+//             }
+//         </div>
+//     )
+// }
 
-export default page
+// export default page
